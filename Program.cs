78c8d9ebf52a4c7081b43dc11154e929ae515382
701030552 <701030552@2260I-L29-031>
2025-03-21 14:44:41 -0400

    not done

--- Program.cs
+++ Program.cs
@@ -73,64 +73,76 @@ namespace Topic_6_loops
         public static void BankOfBlorb()
         {
             string bankchoice;
-            double balance = 150, depositAmount;
+            double balance = 150, depositAmount, transactionfee = 0.75, withdrawalAmount;
 
 
             Console.WriteLine("You chose option 2");
             Console.WriteLine("Hit ENTER to continue.");
             Console.Clear();
 
-            Console.WriteLine("Welcome to the Bank of Blorb");
-            Console.WriteLine();
-            Console.WriteLine("You'll be charged $0.75 with each transaction");
-            Console.WriteLine();
-            Console.WriteLine("Hit Enter to see the available transactions ->");
-            Console.ReadLine();
-            Console.WriteLine("Available transactions: ");
-            Console.WriteLine();
-            Console.WriteLine("Desposit");
-            Console.WriteLine("Withdrawal");
-            Console.WriteLine("Bill payment");
-            Console.WriteLine("Account balance update");
-            Console.WriteLine("Quit");
-
-            bankchoice = Console.ReadLine().ToLower().Trim();
-            Console.WriteLine();
-            if (bankchoice == "deposit")
+            while (true)
             {
-                Console.WriteLine("Enter the amount you wish to deposit: ");
-                while (double.TryParse(Console.ReadLine(), out depositAmount) || depositAmount > 0)
-                    Console.WriteLine("Invalid number: you have $150 in your account");
-                if (amount > 0 || amount > 150)
-                    currentamount = amount - 0.75;
-                Console.WriteLine(value: $"your current amount {currentamount}");
-                Console.WriteLine("");
-            }
-            else if (bankchoice == "withdrawal")
-            {
-
-                
-
-
-            }
-            // Add an else if for each valid choice...
-            else
-            {
-                Console.WriteLine("Invalid choice, press ENTER to continue.");
+                Console.WriteLine("Welcome to the Bank of Blorb");
+                Console.WriteLine();
+                Console.WriteLine("You'll be charged $0.75 with each transaction");
+                Console.WriteLine();
+                Console.WriteLine("Hit Enter to see the available transactions ->");
                 Console.ReadLine();
+                Console.WriteLine("Available transactions: ");
+                Console.WriteLine();
+                Console.WriteLine("Deposit");
+                Console.WriteLine("Withdrawal");
+                Console.WriteLine("Bill payment");
+                Console.WriteLine("Account balance update");
+                Console.WriteLine("Quit");
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.Write("Please type in the transactions you wish to proceed with: ");
+                bankchoice = Console.ReadLine().ToLower().Trim();
+                Console.WriteLine();
+                Console.Clear();
+                if (bankchoice == "deposit")
+                {
+                    Console.WriteLine("Enter the amount you wish to deposit: ");
+                    while (!double.TryParse(Console.ReadLine(), out depositAmount) && depositAmount > 0)
+                        Console.WriteLine("Invalid number: you have $150 in your account");
+                    if (depositAmount >= 0)
+                        balance += depositAmount;
+                    balance -= transactionfee;
+
+                    Console.WriteLine(value: $"your deposit amount: {depositAmount:C} and your current balance: {balance:C}.");
+                    Console.WriteLine();
+                    Console.WriteLine("Thank you for using BOB bank. Hit Enter to return back to menu");
+                    Console.ReadLine();
+                    Console.Clear();
+                    
+                }
+                
+                // Add an else if for each valid choice...
+                else if (bankchoice == "withdrawal")
+                {
+                   
+                    {
+                        Console.WriteLine("Enter the amount you wish to withdraw");
+                        while (!double.TryParse(Console.ReadLine(), out withdrawalAmount) && withdrawalAmount > 0)
+                            Console.WriteLine("Invalid number");
+                        if (withdrawalAmount >= 0 && balance >= 150)
+                            withdrawalAmount -= balance;
+                            balance = withdrawalAmount - transactionfee;
+                        Console.WriteLine($"Your withdrawal amount {withdrawalAmount:C} and your current balance {balance:C}. ");
+                        Console.WriteLine();
+                        Console.WriteLine("Thank you for using BOB bank. Hit Enter to return back to menu");
+                        Console.ReadLine();
+                        Console.Clear();
+                       
+                    }
+              
+                }
             }
         }
 
-
-        public static void Transaction()
-        {
-            int amount;
-            double currentamount = 0;
-            
-
-            
-
-        }
+        
+        
             
             
 
